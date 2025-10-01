a41f2c9f0fd6ad798ec00130ab0107002a7d4fd2
Happya <axlu810+github@gmail.com>
2025-10-01 21:42:58 +0000

    fixed entry point

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,6 @@
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from './App'
-render(<App />, document.getElementById('root'))
+
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
